a59518225dcfbd9361cc4d7f692e5d09b0bc92f5
AshTorosyan <torosyanashotjs@gmail.com>
2022-07-06 17:16:51 +0400

    improved index.js file

--- game/src/App.js
+++ game/src/App.js
@@ -1,9 +1,11 @@
+import { useState } from "react"
 import Wrapper from "./ui/Wrapper/Wrapper"
 
 const App = () => {
+  const [balance,setBalance] = useState('')
   return (
     <Wrapper>
-      
+
     </Wrapper>
   )
 }
